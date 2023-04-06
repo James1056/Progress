e34617a66a518e7b6577ca3cf4f3c8fc66efd562
devinholcomb <111882779+devinholcomb@users.noreply.github.com>
2023-04-06 15:14:48 -0400

    correct progress database

---
+++ progress.sql
@@ -0,0 +1,340 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2023 at 09:13 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `progress`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `user_id` int(11) NOT NULL,
+  `trainer_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gym`
+--
+
+CREATE TABLE `gym` (
+  `id` int(11) NOT NULL,
+  `intro` varchar(5000) NOT NULL,
+  `facility` varchar(5000) NOT NULL,
+  `image` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meal plan`
+--
+
+CREATE TABLE `meal plan` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `text` varchar(1000) NOT NULL,
+  `file` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `meal plan`
+--
+
+INSERT INTO `meal plan` (`id`, `user_id`, `text`, `file`) VALUES
+(1, 3, 'During the week I consume around 3500 calories as I am on a bulk.\r\n\r\nEach day I shall start with a bowl of porridge along with a high calorie protien shake, then I have a snake of a peanut butter sandwich before lunch. For lunch I shall have chicken pasta along with two bananas. After I have worked out I will have a post workout meal of', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `response_user_id` int(11) NOT NULL,
+  `text` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `partner request`
+--
+
+CREATE TABLE `partner request` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `text` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `partner request`
+--
+
+INSERT INTO `partner request` (`id`, `user_id`, `text`) VALUES
+(1, 1, 'Hey guys I\'m new to the gym and am looking for a person to help teach me what I need to do, I want to focus on upper body, thank you.'),
+(2, 2, 'I\'ve been going to the gym for a while now and would really want to work out with a partner.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `user_id` int(11) NOT NULL,
+  `bio` text NOT NULL,
+  `profile_picture` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL,
+  `text` varchar(700) NOT NULL,
+  `user_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`id`, `text`, `user_id`) VALUES
+(16, 'My lower back hurtrs during squats, does anyone know why? any tips?', 0),
+(17, 'my back is disjointed,, what i do', 0),
+(18, 'Can someone help me', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reply`
+--
+
+CREATE TABLE `reply` (
+  `id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `text` varchar(500) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `split`
+--
+
+CREATE TABLE `split` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `text` varchar(1000) NOT NULL,
+  `title` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `split`
+--
+
+INSERT INTO `split` (`id`, `user_id`, `text`, `title`) VALUES
+(14, 0, 'Im gonna teach you how. follow this workout', 'Bench 400'),
+(15, 0, 'first this and that then this, then that', 'Squat 500'),
+(16, 0, 'eat food, lift weight', 'Get massive');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trainer`
+--
+
+CREATE TABLE `trainer` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `gym_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `training video`
+--
+
+CREATE TABLE `training video` (
+  `id` int(11) NOT NULL,
+  `trainer_id` int(11) NOT NULL,
+  `video` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `D.O.B` date NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `fitness_level` int(11) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `dob` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `first_name`, `last_name`, `D.O.B`, `gender`, `fitness_level`, `password`, `dob`) VALUES
+(1, 'sam03lift', 'Samantha ', 'Bailey', '2004-02-11', 'F', 2, 'test123', NULL),
+(2, 'coop123', 'Gerald ', 'Cooper', '2003-01-06', 'M', 1, 'test2', NULL),
+(3, '245Shar245', 'Sharon', 'Harold', '1996-07-12', 'F', 3, 'Test!3', NULL),
+(4, 'AKing927', 'Adam', 'King', '1999-05-01', 'M', 4, 'Trainer123', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gym`
+--
+ALTER TABLE `gym`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `meal plan`
+--
+ALTER TABLE `meal plan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `partner request`
+--
+ALTER TABLE `partner request`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reply`
+--
+ALTER TABLE `reply`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `split`
+--
+ALTER TABLE `split`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trainer`
+--
+ALTER TABLE `trainer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `training video`
+--
+ALTER TABLE `training video`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gym`
+--
+ALTER TABLE `gym`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `meal plan`
+--
+ALTER TABLE `meal plan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `partner request`
+--
+ALTER TABLE `partner request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `reply`
+--
+ALTER TABLE `reply`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `split`
+--
+ALTER TABLE `split`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `trainer`
+--
+ALTER TABLE `trainer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `training video`
+--
+ALTER TABLE `training video`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
